34b29b3789542443b82e5880ce819588b26eb822
NEERAJ NIGAM <99023015+neerajnigam0809@users.noreply.github.com>
2023-09-12 04:08:31 +0530

    Add files via upload

---
+++ src/components/home/Home.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import "./Home.css"
+import home from "../../pictures/home.png"
+import lock from "../../pictures/lock.png"
+
+function Home() {
+  return (
+    <div className='l_home'>
+      <img src={home} alt="home" />
+      <h1>Pocket Notes</h1>
+      <p>Send and receive messages without keeping your phone online.<br/>Use Pocket Notes on up to 4 linked Devices and 1 mobile phone.</p>
+      <div className="l_home_bottom">
+        <img src={lock} alt="lock" />
+        <span>end-to-end encrypted</span>
+      </div>
+    </div>
+  )
+}
+export default Home
